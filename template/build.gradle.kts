f9194db91bd172c47e1994cf32ea85c5df023766
PHILGAMER <73665072+PHILGAMER@users.noreply.github.com>
2021-11-30 13:45:27 +0100

    Add files via upload

---
+++ template/build.gradle.kts
@@ -0,0 +1,28 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.5.10"
+}
+
+group = "de.philgamer"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots")
+
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation("net.axay:kspigot:1.17.4")
+    compileOnly("org.spigotmc:spigot-api:1.17-R0.1-SNAPSHOT")
+}
+
+tasks.test {
+    useJUnit()
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "1.8"
+}
